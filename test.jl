1851c9c9f2e0ae966d40d808e64f7f3ed740e5fe
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2020-04-13 02:37:18 +0200

    major revision (little bugs still present)

--- test.jl
+++ test.jl
@@ -1,44 +1,73 @@
 # imports
 using Random
+using Statistics
 using PyPlot
 
-function compute_norm(agent_list, agent, radius, norm)
+mutable struct CircularBitArray
+    array::BitArray
+    function CircularBitArray(b::BitArray)
+        new(b)
+    end
+end
 
-    # determine base neighborhood
-    lower_bound = (agent - radius) > 0 ? (agent - radius) : 1
-    upper_bound = (agent + radius) > length(agent_list) ? length(agent_list) : agent + radius
-    base_neighborhood = deepcopy(agent_list[lower_bound:upper_bound])
+CircularBitArray(n::Int64) = CircularBitArray(Random.bitrand(n))
 
-    # account for potential overflows
-    lower_tail = agent_list[1:(agent + radius - length(agent_list))]
-    upper_tail = agent_list[((length(agent_list) + 1) - abs(agent - radius)):length(agent_list)]
+Base.:length(c::CircularBitArray) = Base.length(c.array)
 
-    # concatenate all fragments
-    neighborhood = vcat(base_neighborhood, lower_tail, upper_tail)
+function get_k_neighborhood(circ_bit_arr::CircularBitArray, index, k)
 
-    # tie breaking mechanism (keep current norm in case of a tie
-    return (sum(neighborhood) / length(neighborhood))
+    # catch more than entire array case
+    if k > (length(circ_bit_arr) / 2) - 1
+        error("Subsetting with the chosen k exceeds the length of the CircularBitArray.")
+    end
 
+    if index - k < 1  # underflow
+        lower_bound_low = 1
+        upper_bound_low = index + k
+        lower_bound_high = length(circ_bit_arr) - abs(index - k)
+        upper_bound_high = length(circ_bit_arr)
+        return vcat(
+            deepcopy(circ_bit_arr.array[lower_bound_high:upper_bound_high]),
+            deepcopy(circ_bit_arr.array[lower_bound_low:upper_bound_low])
+        )
+    elseif index + k > length(circ_bit_arr)  # overflow
+        lower_bound_low = 1
+        upper_bound_low = abs(index + k) - length(circ_bit_arr)
+        lower_bound_high = index - k
+        upper_bound_high = length(circ_bit_arr)
+        return vcat(
+            deepcopy(circ_bit_arr.array[lower_bound_high:upper_bound_high]),
+            deepcopy(circ_bit_arr.array[lower_bound_low:upper_bound_low])
+        )
+    else  # regular case
+        return circ_bit_arr.array[index - k:index + k]
+    end
+
+end
+
+function compute_norm(agent_list::CircularBitArray, agent::Int64, radius::Int64)
+    return Statistics.mean(get_k_neighborhood(agent_list, agent, radius))
 end
 
 function update_radius!(radius_list, agent_list, current_agent, tolerance)
 
     # current properties
     current_radius = radius_list[current_agent]
-    current_norm = agent_list[current_agent]
+    current_norm = agent_list.array[current_agent]
 
-    # update radius
-    if abs(compute_norm(agent_list, current_agent, current_radius, current_norm)
-        - compute_norm(agent_list, current_agent, current_radius + 1, current_norm)) > tolerance
+    if (current_radius < (length(agent_list) \ 2) - 1 & current_radius > 1)
 
-        if current_radius < length(agent_list)
+
+        # update radius
+        if (
+            abs(compute_norm(agent_list, current_agent, current_radius)
+            - compute_norm(agent_list, current_agent, current_radius + 1)) > tolerance
+        )
             radius_list[current_agent] += 1
-        end
-
-    elseif abs(compute_norm(agent_list, current_agent, current_radius, current_norm)
-        - compute_norm(agent_list, current_agent, current_radius - 1, current_norm)) < tolerance
-
-        if current_radius > 1
+        elseif (
+            abs(compute_norm(agent_list, current_agent, current_radius)
+            - compute_norm(agent_list, current_agent, current_radius - 1)) < tolerance
+        )
             radius_list[current_agent] -= 1
         end
 
@@ -51,18 +80,18 @@ end
 function update_norm!(agent_list, radius_list, current_agent, noise=0.0)
 
     current_radius = radius_list[current_agent]
-    current_norm = agent_list[current_agent]
+    current_norm = agent_list.array[current_agent]
 
-    computed_norm = compute_norm(agent_list, current_agent, current_radius, current_norm)
+    computed_norm = compute_norm(agent_list, current_agent, current_radius)
 
     if computed_norm > 0.5
-        agent_list[current_agent] = true
+        agent_list.array[current_agent] = true
     elseif computed_norm < 0.5
-        agent_list[current_agent] = false
+        agent_list.array[current_agent] = false
     end
 
     if Random.rand() < noise
-        agent_list[current_agent] = Random.bitrand(1)[1]
+        agent_list.array[current_agent] = Random.bitrand(1)[1]
     end
 
     return agent_list
@@ -71,16 +100,22 @@ end
 
 mutable struct Config
 
-    agent_list::BitArray
+    agent_list::CircularBitArray
     radius_list::Array{Int64, 1}
     tolerance::Float64
     sim_iterations::Int64
     noise::Float64
 
-    function Config(agent_count, max_radius, tolerance, sim_iterations, noise=0.0)
+    function Config(
+        circ_bit_arr::CircularBitArray,
+        max_radius,
+        tolerance,
+        sim_iterations,
+        noise=0.0
+    )
         new(
-            Random.bitrand(agent_count),
-            Random.rand(1:max_radius, agent_count),
+            circ_bit_arr,
+            Random.rand(1:max_radius, length(circ_bit_arr)),
             tolerance,
             sim_iterations,
             noise
@@ -103,18 +138,18 @@ end
 
 function simulate(config; display=true)
 
-    state = (config.agent_list, config.radius_list)
+    state = (deepcopy(config.agent_list), deepcopy(config.radius_list))
     norm_matrix = zeros(Int64, config.sim_iterations + 1, length(config.agent_list))
     radius_matrix = zeros(Int64, config.sim_iterations + 1, length(config.agent_list))
 
     # add initial state to matrices
-    norm_matrix[1, :] = state[1]
+    norm_matrix[1, :] = state[1].array
     radius_matrix[1, :] = state[2]
 
     # simulation
     for i in 1:config.sim_iterations
         new_state = tick!(state, config)
-        norm_matrix[i + 1, :] = deepcopy(new_state[1])
+        norm_matrix[i + 1, :] = deepcopy(new_state[1].array)
         radius_matrix[i + 1, :] = deepcopy(new_state[2])
     end
     results = (norm_matrix, radius_matrix)
@@ -154,5 +189,20 @@ function display_results(results)
 end
 
 # simulation runs
-cfg = Config(191, 30, 0.001, 200, 1.0)
-results = simulate(cfg)
+cfg = Config(
+    CircularBitArray(50),
+    15,
+    0.05,
+    10,
+    0.8
+)
+cfg2 = Config(
+    CircularBitArray(BitArray(zeros(50))),
+    15,
+    0.05,
+    100,
+    0.8
+)
+results = simulate(cfg2)
+
+# TO DO: debug update_radius!() 
